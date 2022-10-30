751f3a826fcffd2c6f2520c4168d64510924e4eb
tkumakiri <ogdrenyz0117@gmail.com>
2022-10-30 18:18:42 +0900

    scractch memo page

--- frontend/src/components/pages/Memo.tsx
+++ frontend/src/components/pages/Memo.tsx
@@ -8,11 +8,17 @@ export default function MemoPage() {
     setMemo(e.target.value);
   };
   return (
-    <div>
-      <div>メモ欄</div>
-      <input type="text" onChange={handleChangeMemo} />
-      <div>{memo}</div>
-      <div>カテゴリ</div>
+    <div className="grid justify-items-center h-screen">
+      <div className="font-serif space-y-2 bg-slate-300 w-3/5 h-3/5 mt-2.5">
+        <div className="mx-4">
+          <h1 className="text-xl">To DO memo</h1>
+          <div className="my-10">
+            <div>メモ欄</div>
+            <input className="border border-gray-300" type="text" onChange={handleChangeMemo} />
+          </div>
+          <div>カテゴリ</div>
+        </div>
+      </div>
     </div>
   );
 }
